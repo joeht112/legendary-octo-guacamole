0185b7839fb0081b558825082110f6ff24b3f223
Joe Thewlis <joethewlis@live.co.uk>
2024-11-14 04:24:32 +0000

    add first 5 questions

---
+++ script.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+        question: "Where do the band ABBA originally come from?",
+        answers: [
+            {text: "Denmark", correct: false},
+            {text: "Sweden", correct: true},
+            {text: "Norway", correct: false},
+            {text: "Finland", correct: false},
+        ]
+    },
+    {
+        question: "Who is the frontman for the band 'Metallica'?",
+        answers: [
+            {text: "Lars Ulrich", correct: false},
+            {text: "James Hetfield", correct: true},
+            {text: "Robert Trujillo", correct: false},
+            {text: "Kirk Hammett", correct: false},
+        ]
+    },
+    {
+        question: "In what year did Dua Lipa win her first Grammy?",
+        answers: [
+            {text: "2016", correct: false},
+            {text: "2017", correct: false},
+            {text: "2018", correct: false},
+            {text: "2019", correct: true},
+        ]
+    },
+    {
+        question: "How many members are in the band 'Red Hot Chilli Peppers",
+        answers: [
+            {text: "3", correct: false},
+            {text: "4", correct: true},
+            {text: "5", correct: false},
+            {text: "6", correct: false},
+        ]
+    },
+    {
+        question: "Who was the lead singer of 'No Doubt'?",
+        answers: [
+            {text: "Gwen Stefani", correct: true},
+            {text: "Christina Aguilera", correct: false},
+            {text: "Debbie Harry", correct: false},
+            {text: "Emma Bunton", correct: false},
+        ]
+    }
+]
